7a82d55b689d8b3723ff7c0ead03ebad0001f77e
chens <cscfufo@163.com>
2024-02-05 15:28:09 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,7 +1,7 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-  { text: '前端导航', link: '/nav/' },
+  { text: '网络安全导航', link: '/nav/' },
   { text: 'cs主页', link: 'https://ufofly.github.com/vitepress' }
  
   
