e0444c0a702d003ef4bb210aa3ab19dfdd243223
chens <cscfufo@163.com>
2024-02-05 16:50:03 +0800

    test

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -30,24 +30,34 @@ export const NAV_DATA: NavData[] = [
         link: 'https://dh.aabyss.cn/',
       },
       {
-        icon: '/icons/json-cn.ico',
+        icon: '',
         title: '极牛导航',
         link: 'https://hao.geeknb.com/',
       },
     ],
   },
   {
-    title: 'AI 导航',
+    title: '安全团队博客',
     items: [
       {
-        icon: '/icons/chatgpt.png',
-        title: 'ChatGPT（最强）',
-        link: 'https://chat.openai.com/chat',
+        icon: 'https://blog.xlab.qianxin.com/content/images/size/w256h256/2023/10/xlab-icon-1.png',
+        title: '奇安信X实验室',
+        link: 'https://blog.xlab.qianxin.com/',
       },
       {
-        icon: 'https://www.notion.so/images/logo-ios.png',
-        title: 'Notion AI（笔记）',
-        link: 'https://www.notion.so',
+        icon: 'https://blog.netlab.360.com/content/images/size/w256h256/2019/02/netlab_xs-2.png',
+        title: '360netlab实验室',
+        link: 'https://blog.netlab.360.com/',
+      },
+      {
+        icon:'https://www.trendmicro.com/content/dam/trendmicro/favicon.ico',
+        title:'趋势科技',
+        link: 'https://www.trendmicro.com/en_us/research.html',
+      },
+      {
+        icon: 'https://paper.seebug.org/static/images/favicon.ico',
+        title: 'Seebug',
+        link: 'https://paper.seebug.org/',
       },
      
     ],
